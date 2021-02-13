3e788fb38b6ed91d5a8a1a1cb90597e99e30537c
Amir Hussein <36240131+AmirHussein96@users.noreply.github.com>
2021-02-13 17:44:03 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,57 @@ from functools import partial
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from mpl_toolkits.axes_grid1 import ImageGrid
 import pandas as pd
+import seaborn as sns
 import os
 
+#matplotlib.use("TkAgg")
+
+def compare_recon(sess,train,model,ch,save_fig):
+    s=sess.run([model.decoder_t],feed_dict = {model.X_t_n:train})
+    reconstructed=np.array([i-i.mean(0) for i in s[0]])
+   # reconstructed=np.array([(j-j.min(0))/(j.max(0)-j.min(0)) for j in s[0]])
+    #reconstructed=s[0]
+    train=np.array([n-n.mean(0) for n in train])
+   # train=np.array([(j-j.min(0))/(j.max(0)-j.min(0)) for j in train])
+ 
+    
+    #plt.title('Training Source Images')
+    ints=np.random.randint(0,len(train),6)
+    for i in ints:
+        plt.ion()
+        # plot original and reconstructed
+        x = np.arange(128)
+        fig, (ax1) = plt.subplots(1, 1, figsize=(10, 4))
+        sns.lineplot(x,reconstructed[i, :, ch], color='green', alpha=0.7, label='Reconstructed', ax=ax1)
+        ax1.lines[0].set_linestyle("--")
+        sns.lineplot(x,train[i, :, ch], color='red', label='Original', ax=ax1)
+        
+        ax1.legend(loc='upper right')
+        #
+        if save_fig==True:
+            fig.savefig('{}.png'.format(i))
+        plt.ioff()
+        plt.show()
+
+
+
+
+def gen_noise(shape, x, zero_data = False, scale=0.15):
+    if not zero_data:
+        return np.random.normal(loc=0, scale=scale, size=shape)
+    # zero out some values
+    zeros = np.zeros(shape)
+    unif_n = np.arange(x.size)/(x.size-1)
+    np.random.shuffle(unif_n)
+    # percentage of frames to keep, 1-keep will be set to zero
+    keep = 0.97
+    mask = (unif_n>keep).reshape(shape)
+    x[mask] = zeros[mask]
+    # -x will zero out 1-keep % of elements
+    return -x
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
